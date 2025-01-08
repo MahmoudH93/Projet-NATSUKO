4be013e50e4bc9217bff10e05d151bd46b47bdb2
yanishimidi <150398346+yanishimidi@users.noreply.github.com>
2025-01-08 17:53:45 +0100

    Add files via upload
    serveur tcp version final

---
+++ serveur.py
@@ -0,0 +1,35 @@
+import socket
+import select
+
+serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host, port = '127.0.0.1', 2025
+serveur.bind((host, port))
+serveur.listen(4)
+client_connectee = True
+socket_objs = [serveur]
+print("bienvenue dans le chat!")
+
+while client_connectee:
+    liste_lu, liste_acce_Ecrit, Exeption = select.select(socket_objs, [], socket_objs)
+    for socket_obj in liste_lu:
+        if socket_obj is serveur:
+            client, adresse = serveur.accept()
+            socket_objs.append(client)
+
+        else:
+            donnée_reçu = socket_obj.recv(128).decode("utf-8")
+
+            if donnée_reçu:
+                print(donnée_reçu)
+
+            else:
+                socket_objs.remove(socket_obj)
+                socket_obj.close()
+                print("1 participant déconnecté")
+                print(f"{len(socket_objs) - 1} participant(s) restant(s)")
+
+
+
+
+
+
